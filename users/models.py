c4b30819bae50650eee449782261cf36c89d3d15
Kamikoto16 <91416496+Kamikoto16@users.noreply.github.com>
2023-06-20 17:48:31 +0700

    таблица status
    создал таблицу статус для постов
    
    она необходима для установки статусов
    
    - опубликовано
    - модерация
    - архив

--- users/models.py
+++ users/models.py
@@ -22,7 +22,14 @@ class type_of_animal(models.Model):
         verbose_name = 'вид'
         verbose_name_plural = 'вид'
         ordering = ['id']
-
+class status(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name="вид")    
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name = 'status'
+        verbose_name_plural = 'status'
+        ordering = ['id']
 class posts(models.Model):
     id_user = models.IntegerField()
     title = models.CharField(max_length=255, verbose_name="Заголовок")
@@ -34,6 +41,7 @@ class posts(models.Model):
     is_published = models.BooleanField(default=True, verbose_name="Публикация")
     category = models.ForeignKey('Category', on_delete=models.PROTECT, verbose_name="Категории")
     type_of_animal = models.ForeignKey('type_of_animal', on_delete=models.PROTECT, verbose_name="вид")
+    status = models.ForeignKey('status', on_delete=models.PROTECT, verbose_name="status")
 
 
 
